e2912b0adb880e0255f7cc1e1388e9093ab732dc
Necip <necipsaylan@gmail.com>
2021-03-11 00:20:57 +0300

    20210310

--- homework/homework1.py
+++ homework/homework1.py
@@ -1,11 +1,13 @@
 odd = [1, 3, 5, 7, 9]
 even = [0, 2, 4, 6, 8]
-
-for a in zip (odd,even):
-    print(list(a))
-    list2=a*2
-    print(list2)
-    #print(f'Even: {even[a]*2}')
-    #print(f'Odd: {odd[a]*2}')
+oddm = []
+evenm = []
+for i in odd:
+    oddm.append(i*2)
+for i in even:
+    evenm.append(i*2)
+for a in zip (oddm,evenm):
+    print(a)
+    print(type(a))
     
 
